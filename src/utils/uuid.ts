5a0ac02304faf30a336ba2ba3f07cd4a235010d6
Yudan <yudan.lin@hpe.com>
2018-05-11 11:12:42 +0800

    some update

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -1,7 +1,7 @@
 export function uuid():string{
   var s = [];
   var hexDigits = "0123456789abcdef";
-  for (var i = 0; i < 5; i++) {
+  for (var i = 0; i < 16; i++) {
       s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
   }
   return s.join("");
